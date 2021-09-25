abcbbd2f1357c82dba4a3a556f258a2c377be724
gotti <gotti@mail.gotti.dev>
2021-09-25 19:53:13 +0900

    compress images

--- compress.sh
+++ compress.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 jpg_files=`find ./www/content/post -type f -name "*.jpg"`
 for image in $jpg_files; do
-  webp_image=`echo $jpg_files | sed -e "s/jpg^/webp/"`
-  magick $jpg_files $webp_image
+  webp_image=`echo $image | sed -e "s/jpg\$/webp/"`
+  echo "$webp_image"
+  magick convert -quality 85 -resize 70% $image $webp_image
+  find . -name "*.md" | xargs sed -i "s/$(basename $image)/$(basename $webp_image)/g"
   rm $image
 done
