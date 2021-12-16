f89cef3d27d9c6bff41143394c15e04046072083
gotti <gotti@mail.gotti.dev>
2021-12-16 21:53:24 +0900

    add ogp

--- www/frontend/components/PageHead/PageHead.tsx
+++ www/frontend/components/PageHead/PageHead.tsx
@@ -11,6 +11,8 @@ export const PageHead :FC<Props> = ( { title } ) => {
       <title>{title} ごっちでぶ</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="og:title" content={title} />
+      <meta name="og:site_name" content="ごっちでぶ" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@0xg0tti" />
     </Head>
