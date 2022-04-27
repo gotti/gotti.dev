18994ffc4793a2e9ee5b5625c4ca85e59dff463e
gotti <gotti@mail.gotti.dev>
2022-04-27 22:09:07 +0900

    update iroiro

--- www/frontend/pages/_app.tsx
+++ www/frontend/pages/_app.tsx
@@ -1,17 +1,11 @@
 import {FC} from "react";
 import {AppProps} from "next/app";
 import {Menu} from "../components/Menu";
-import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
 import "./app.scss";
 
 const App: FC<AppProps> = ({Component, pageProps}) => {
   return (
     <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: initTwitterScriptInner
-        }}
-      />
       <main>
         <div className="Menu">
           <Menu />
